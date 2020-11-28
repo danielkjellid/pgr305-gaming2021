eb30fe84a4896ca1bfad3160b8809e4bdb8dae9c
danielkjellid <daniel.kjellid@gmail.com>
2020-11-28 22:47:17 +0100

    Started working on edit processes

--- src/views/Admin.js
+++ src/views/Admin.js
@@ -11,12 +11,45 @@ import Image from 'react-bootstrap/Image'
 
 const Admin = () => {
 
+  // data for the time being
   const [dummyData] = useState([{ id: 1, image: 'https://www.elkjop.no/primaryimage/193479', title: 'Fifa 21', category: 'Sport', price: 599}, { id: 2, title: 'Bloodborne', image: 'https://www.elkjop.no/image/dv_web_D180001002318523/PS4HITS6/bloodborne-ps4.jpg?$prod_all4one$', category: 'Fantasy', price: 199}])
-  const [key, setKey] = useState('games')
-  const [showConfirmDelete, setShowConfirmDelete] = useState(false)
+  const [clickedItem, setClickedItem] = useState([])
 
-  const handleConfirmDeleteClose = () => setShowConfirmDelete(false)
-  const handleConfirmDeleteShow = () => setShowConfirmDelete(true)
+  // state controlling tabs
+  const [key, setKey] = useState('games')
+
+  // delete instance states and handlers
+  const [showConfirmDeleteModal, setShowConfirmDeleteModal] = useState(false)
+
+  const handleConfirmDeleteModalClose = () => setShowConfirmDeleteModal(false)
+  const handleConfirmDeleteModalShow = (id) => {
+    // save clickedItem in variable
+    const clickedItem = dummyData.filter(instance => instance.id === id)
+    
+    // set states
+    setClickedItem(clickedItem)
+    setShowConfirmDeleteModal(true)
+  }
+
+  const handleDeleteItem = (id) => {
+    const itemIndex = clickedItem.findIndex((element) => element.id === id)
+    dummyData.splice(itemIndex, 1)
+    setShowConfirmDeleteModal(false)
+  }
+
+  // edit instance states and handlers
+  const [showEditModal, setShowEditModal] = useState(false)
+
+  const handleEditModalClose = () => setShowEditModal(false)
+  const handleEditModalShow = (id) => {
+    // save clickedItem in variable
+    const clickedItem = dummyData.filter(instance => instance.id === id)
+
+    // set states
+    setClickedItem(clickedItem)
+    setShowEditModal(true)
+  }
+
 
   return (
     <div>
@@ -45,7 +78,7 @@ const Admin = () => {
                   </tr>
                 </thead>
                 <tbody>
-                  {dummyData.map((data => 
+                  {dummyData.map(data => 
                     <tr key={data.id}>
                       <td><Image className="product-image-container" src={data.image} rounded /></td>
                       <td>{data.id}</td>
@@ -54,18 +87,12 @@ const Admin = () => {
                       <td>{data.price}</td>
                       <td>
                         <ButtonGroup>
-                          <Button variant="secondary">
-                            <svg className="btn-icon" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
-                              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
-                            </svg>
-                          </Button>
-                          <Button variant="secondary">
+                          <Button variant="secondary" onClick={() => handleEditModalShow(data.id)}>
                             <svg className="btn-icon" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z" />
                             </svg>
                           </Button>
-                          <Button variant="danger" onClick={handleConfirmDeleteShow}>
+                          <Button variant="danger" onClick={() => handleConfirmDeleteModalShow(data.id)}>
                             <svg className="btn-icon" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
                             </svg>
@@ -73,7 +100,7 @@ const Admin = () => {
                         </ButtonGroup>
                       </td>
                     </tr>
-                  ))}
+                  )}
                 </tbody>
               </Table>
             </Tab>
@@ -82,20 +109,46 @@ const Admin = () => {
             </Tab>
           </Tabs>
         </Container>
-        <Modal animation={false} show={showConfirmDelete} onHide={handleConfirmDeleteClose}>
-          <Modal.Header closeButton>
-            <Modal.Title>Delete instance</Modal.Title>
-          </Modal.Header>
-          <Modal.Body>Are you sure yout want to delete name? This action cannot be undone.</Modal.Body>
-          <Modal.Footer>
-            <Button variant="secondary" onClick={handleConfirmDeleteClose}>
-              No
-            </Button>
-            <Button variant="danger" onClick={handleConfirmDeleteClose}>
-              Delete instance
-            </Button>
-          </Modal.Footer>
-        </Modal>
+        {clickedItem.map((item) =>
+          <div key={item.id}>
+            <Modal
+              animation={false} 
+              show={showConfirmDeleteModal} 
+              onHide={handleConfirmDeleteModalClose}
+            >
+                <Modal.Header closeButton>
+                  <Modal.Title>Delete instance</Modal.Title>
+                </Modal.Header>
+                  <Modal.Body>Are you sure you want to delete {item.title}? This action cannot be undone.</Modal.Body>
+                <Modal.Footer>
+                  <Button variant="secondary" onClick={handleConfirmDeleteModalClose}>
+                    No
+                  </Button>
+                  <Button variant="danger" onClick={() => handleDeleteItem(item.id)}>
+                    Delete instance
+                  </Button>
+                </Modal.Footer>
+            </Modal>
+            <Modal
+              animation={false} 
+              show={showEditModal} 
+              onHide={handleEditModalClose}
+            >
+                <Modal.Header closeButton>
+                  <Modal.Title>Edit {item.title}</Modal.Title>
+                </Modal.Header>
+                  <Modal.Body>X</Modal.Body>
+                <Modal.Footer>
+                  <Button variant="secondary" onClick={handleEditModalClose}>
+                    Cancel
+                  </Button>
+                  <Button variant="success" onClick={() => handleDeleteItem(item.id)}>
+                    Save
+                  </Button>
+                </Modal.Footer>
+            </Modal>
+          </div> 
+        )}
       </div>
     </div>
   )
