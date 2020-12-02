1db483ba2db67789d8db00275c87302f9867e565
danielkjellid <daniel.kjellid@gmail.com>
2020-12-03 00:38:49 +0100

    Fixed characters endpoint

--- backend/Startup.cs
+++ backend/Startup.cs
@@ -38,7 +38,8 @@ namespace backend
             services.AddSingleton<IBackendstoreDatabaseSettings>(
                 sp => sp.GetRequiredService<IOptions<BackendstoreDatabaseSettings>>().Value
             );
-
+            
+            services.AddSingleton<CharactersService>();
             services.AddSingleton<GamesService>();
         }
 
