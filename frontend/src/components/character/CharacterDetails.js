37dae3fc76a36fe4f5334ec762bc0d533fa295ce
danielkjellid <daniel.kjellid@gmail.com>
2020-12-04 00:35:02 +0100

    Styling of characters and games

--- frontend/src/components/character/CharacterDetails.js
+++ frontend/src/components/character/CharacterDetails.js
@@ -1,59 +1,63 @@
 import React from 'react'
 import { useParams, Link } from 'react-router-dom'
 import { useAsyncStateContext } from '../../context/AsyncStateContext'
+import { Card } from 'react-bootstrap'
 
 const CharacterDetails = ({ data }) => {
   const { gameState } = useAsyncStateContext()
   const { characterId } = useParams()
   const character = data?.find((c) => c.id === String(characterId))
   let characterData
-  console.log(character?.gamesId)
 
   if (character) {
     characterData = (
       <>
-        <div>
-          <Link to='/characters'>Back to characters</Link>
-        </div>
-        <div className='character-details'>
-          <img
-            className='character-image'
-            src={character.image}
-            alt='Character portrait'
-          />
-          <div className='character-details-desc'>
-            <h2> {character.name} </h2>
-            <p>
-              <span className='desc-gray'> Gender: </span> {character.gender}
-            </p>
-            <p>
-              <span className='desc-gray'> Homeworld: </span>
-              {character.homeWorld}
-            </p>
-            <div className='games-including-character'>
-              <h5>Appears in:</h5>
-              <div className='character-gamelist'>
-                {character?.gamesId.map((id) =>
-                  gameState.data
-                    ?.filter((game) => game.id === id)
-                    .map((filteredGame) => (
-                      <Link to={`/games/${filteredGame.id}`}>
-                        <img
-                          alt='Game cover'
-                          key={filteredGame.id}
-                          src={filteredGame.image}
-                          style={{
-                            width: '150px',
-                            marginRight: '10px',
-                          }}
-                        ></img>
-                      </Link>
-                    ))
-                )}
+      <div>
+        <Link to='/characters'>Back to characters</Link>
+      </div>
+      <Card className="mt-4 no-hover">
+        <Card.Body>
+          <div className='character-details'>
+            <img
+              className='character-image'
+              src={character.image}
+              alt='Character portrait'
+            />
+            <div className='character-details-desc'>
+              <h2> {character.name} </h2>
+              <p>
+                <span className='desc-gray'> Gender: </span> {character.gender}
+              </p>
+              <p>
+                <span className='desc-gray'> Homeworld: </span>
+                {character.homeWorld}
+              </p>
+              <div className='games-including-character'>
+                <h5>Appears in:</h5>
+                <div className='character-gamelist'>
+                  {character?.gamesId.map((id) =>
+                    gameState.data
+                      ?.filter((game) => game.id === id)
+                      .map((filteredGame) => (
+                        <Link key={filteredGame.id} to={`/games/${filteredGame.id}`}>
+                          <img
+                            alt='Game cover'
+                            key={filteredGame.id}
+                            src={filteredGame.image}
+                            style={{
+                              width: '150px',
+                              marginRight: '10px',
+                            }}
+                          ></img>
+                        </Link>
+                      ))
+                  )}
+                </div>
               </div>
             </div>
           </div>
-        </div>
+        </Card.Body>
+      </Card>
       </>
     )
   } else {
--- frontend/src/components/game/GameDetails.js
+++ frontend/src/components/game/GameDetails.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { useParams, Link } from 'react-router-dom'
 import CharacterItem from '../../components/character/CharacterItem'
 import { useAsyncStateContext } from '../../context/AsyncStateContext'
-import { Row, Col } from 'react-bootstrap'
+import { Row, Col, Card, Container } from 'react-bootstrap'
 
 const GameDetails = ({ data }) => {
   const { characterState } = useAsyncStateContext()
@@ -17,39 +17,53 @@ const GameDetails = ({ data }) => {
         <div>
           <Link to='/games'>Back to games</Link>
         </div>
-        <div className='game-details'>
-          <div>
-            <img className='game-image' src={game.image} alt='Game cover' />
-          </div>
-          <div className='game-details-desc'>
-            <h2> {game.title} </h2>
-            <p>
-              <span className='desc-gray'>Genre: </span> {game.genre}
-            </p>
-            <p>
-              <span className='desc-gray'>Price: </span> {game.price},-
-            </p>
-            <p>
-              <span className='desc-gray'>Console: </span> {game.console}
-            </p>
-            <p>
-              <span className='desc-gray'>Characters: </span>
-            </p>
-            <Row>
-              {characterState.data
-                ?.filter((character) => character.gamesId.includes(gameId))
-                .map((char) => (
-                  <Col>
-                    <CharacterItem
-                      key={char.id}
-                      character={char}
-                      url={'/characters'}
-                    />
+        <Card className="mt-4 no-hover">
+          <Card.Body>
+            <div className='game-details'>
+              <Container>
+                <Row xs={1}>
+                  <Col xs={{ order: 2}} sm={{ order: 1, span: 6}}>
+                    <div className='game-details-desc'>
+                      <h2> {game.title} </h2>
+                      <p>
+                        <span className='desc-gray'>Genre: </span> {game.genre}
+                      </p>
+                      <p>
+                        <span className='desc-gray'>Price: </span> {game.price},-
+                      </p>
+                      <p>
+                        <span className='desc-gray'>Console: </span> {game.console}
+                      </p>
+                      <p>
+                        <span className='desc-gray'>Characters: </span>
+                      </p>
+                    </div>
                   </Col>
-                ))}
-            </Row>
-          </div>
-        </div>
+                  <Col xs={{ order: 1, span: 6, offset: 3 }} sm={{ order: 2, span: 3 }}>
+                    <div>
+                      <img className='game-image' src={game.image} alt='Game cover' />
+                    </div>
+                  </Col>
+                  <Col xs={{ order: 3 }}>
+                    <Row className="mt-4">
+                      {characterState.data
+                        ?.filter((character) => character.gamesId.includes(gameId))
+                        .map((char) => (
+                          <Col xs={{ order: 3}}>
+                            <CharacterItem
+                              key={char.id}
+                              character={char}
+                              url={'/characters'}
+                            />
+                          </Col>
+                        ))}
+                    </Row>
+                  </Col>
+                </Row>
+              </Container>
+            </div>
+          </Card.Body>
+        </Card>
       </>
     )
   } else {
