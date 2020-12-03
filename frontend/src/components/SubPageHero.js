3b6016a2f038f5cd7e670a4aadd64ffb41493e4f
danielkjellid <daniel.kjellid@gmail.com>
2020-12-03 23:44:46 +0100

    Added overlay to headers

--- frontend/src/components/SubPageHero.js
+++ frontend/src/components/SubPageHero.js
@@ -3,10 +3,11 @@ import { Container } from 'react-bootstrap'
 
 const SubPageHero = ({ heading, page }) => {
   return (
-    <div className={page ? 'game-hero' : 'character-hero'}>
+    <div className={page ? 'hero game-hero' : 'hero character-hero'}>
       <Container>
         <h2>{heading}</h2>
       </Container>
+      <div className="hero-overlay"></div>
     </div>
   )
 }
